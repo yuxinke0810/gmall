5daec369726cb26fd801bef91e9848d855dba60b
于馨轲 <yuxinke.to@foxmail.com>
2019-09-09 17:04:13 +0800

    提交0909

---
+++ gmall-manage-service/src/main/java/com/atguigu/gmall/manage/service/impl/ManageServiceImpl.java
@@ -0,0 +1,92 @@
+package com.atguigu.gmall.manage.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.*;
+import com.atguigu.gmall.manage.mapper.*;
+import com.atguigu.gmall.service.ManageService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class ManageServiceImpl implements ManageService {
+
+    @Autowired
+    private BaseCatalog1Mapper baseCatalog1Mapper;
+
+    @Autowired
+    private BaseCatalog2Mapper baseCatalog2Mapper;
+
+    @Autowired
+    private BaseCatalog3Mapper baseCatalog3Mapper;
+
+    @Autowired
+    private BaseAttrInfoMapper baseAttrInfoMapper;
+
+    @Autowired
+    private BaseAttrValueMapper baseAttrValueMapper;
+
+    /**
+     * 获取所有一级分类数据
+     * @return
+     */
+    @Override
+    public List<BaseCatalog1> getBaseCatalog1() {
+        return baseCatalog1Mapper.selectAll();
+    }
+
+    /**
+     * 根据一级分类Id查询所有二级分类
+     * @param catalog1Id 一级分类Id
+     * @return
+     */
+    @Override
+    public List<BaseCatalog2> getBaseCatalog2(String catalog1Id) {
+        BaseCatalog2 baseCatalog2 = new BaseCatalog2();
+        baseCatalog2.setCatalog1Id(catalog1Id);
+        return baseCatalog2Mapper.select(baseCatalog2);
+//        Example example = new Example(BaseCatalog2.class);
+//        example.createCriteria().andAllEqualTo(catalog1Id);
+//        return baseCatalog2Mapper.selectByExample(example);
+    }
+
+    /**
+     * 根据二级分类Id查询所有三级分类
+     * @param catalog2Id 二级分类Id
+     * @return
+     */
+    @Override
+    public List<BaseCatalog3> getBaseCatalog3(String catalog2Id) {
+        BaseCatalog3 baseCatalog3 = new BaseCatalog3();
+        baseCatalog3.setCatalog2Id(catalog2Id);
+        return baseCatalog3Mapper.select(baseCatalog3);
+    }
+
+    /**
+     * 根据三级分类Id查询平台属性集合
+     * @param catalog3Id 三级分类Id
+     * @return
+     */
+    @Override
+    public List<BaseAttrInfo> getAttrList(String catalog3Id) {
+        BaseAttrInfo baseAttrInfo = new BaseAttrInfo();
+        baseAttrInfo.setCatalog3Id(catalog3Id);
+        return baseAttrInfoMapper.select(baseAttrInfo);
+    }
+
+    @Transactional
+    @Override
+    public void saveAttrInfo(BaseAttrInfo baseAttrInfo) {
+        //保存BaseAttrInfo
+        baseAttrInfoMapper.insertSelective(baseAttrInfo);
+        //保存
+        List<BaseAttrValue> attrValueList = baseAttrInfo.getAttrValueList();
+        if (attrValueList != null && attrValueList.size() > 0) {
+            for (BaseAttrValue baseAttrValue : attrValueList) {
+                baseAttrValue.setAttrId(baseAttrInfo.getId());
+                baseAttrValueMapper.insertSelective(baseAttrValue);
+            }
+        }
+    }
+}
