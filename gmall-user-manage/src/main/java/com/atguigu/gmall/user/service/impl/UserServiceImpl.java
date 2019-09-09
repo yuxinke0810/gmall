d6aafb6e2058385ab2f879bccf2cc7a18b661dac
于馨轲 <yuxinke.to@foxmail.com>
2019-09-09 17:05:43 +0800

    提交0909

---
+++ gmall-user-manage/src/main/java/com/atguigu/gmall/user/service/impl/UserServiceImpl.java
@@ -0,0 +1,34 @@
+package com.atguigu.gmall.user.service.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.atguigu.gmall.bean.UserAddress;
+import com.atguigu.gmall.bean.UserInfo;
+import com.atguigu.gmall.service.UserService;
+import com.atguigu.gmall.user.mapper.UserAddressMapper;
+import com.atguigu.gmall.user.mapper.UserInfoMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserInfoMapper userInfoMapper;
+
+    @Autowired
+    private UserAddressMapper userAddressMapper;
+
+    @Override
+    public List<UserInfo> findAll() {
+        return userInfoMapper.selectAll();
+    }
+
+    @Override
+    public List<UserAddress> getUserAddressList(String userId) {
+        UserAddress userAddress = new UserAddress();
+        userAddress.setId(userId);
+        List<UserAddress> userAddressList = userAddressMapper.select(userAddress);
+        return userAddressList;
+    }
+}
